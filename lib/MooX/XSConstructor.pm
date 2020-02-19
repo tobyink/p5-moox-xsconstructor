9cc766c0d92f504975f20f28d9478452eae743ef
Toby Inkster <mail@tobyinkster.co.uk>
2020-02-19 15:33:46 +0000

    bump version; changelog

--- lib/MooX/XSConstructor.pm
+++ lib/MooX/XSConstructor.pm
@@ -5,7 +5,7 @@ use warnings;
 package MooX::XSConstructor;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.001';
+our $VERSION   = '0.002';
 
 use Moo 1.006000 ();
 use Scalar::Util qw(blessed);
